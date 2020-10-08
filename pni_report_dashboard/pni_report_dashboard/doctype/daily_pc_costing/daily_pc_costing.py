c419d339d89b9e4bbe118f70d020be1e7c2e21e9
Jigar Tarpara <jigartarpara68@gmail.com>
2020-10-08 11:01:59 +0530

    Daily COsting

--- pni_report_dashboard/pni_report_dashboard/doctype/daily_pc_costing/daily_pc_costing.py
+++ pni_report_dashboard/pni_report_dashboard/doctype/daily_pc_costing/daily_pc_costing.py
@@ -35,11 +35,15 @@ class DailyPCCosting(Document):
 					and item.brand = '{brand}'
 					and pni_packing.name = se.pni_reference
 					and se.pni_reference_type = 'PNI Packing'
-		""".format(date=self.date,brand=brand))
+		""".format(date=self.date_for_costing,brand=brand))
 
 		if len(data) and len(data[0]):
 			raw.total_cup_production = data[0][0]
 			raw.net_weight = data[0][1]
+		
+		self.calculate_blank_scrap(raw)
+		self.calculate_bottom_scrap(raw)
+		
 		
 		if not raw.net_weight:
 			raw.net_weight = 0
@@ -65,3 +69,23 @@ class DailyPCCosting(Document):
 			raw.f3 = (float(raw.f1)+float(raw.f2))/ float(raw.total_cup_production)
 		else:
 			raw.f3 = 0
+	
+	def calculate_blank_scrap(self, raw):
+		data = frappe.db.sql(""" 
+			select sum(sed.qty)
+				from 
+					`tabStock Entry Detail` as sed,
+					`tabStock Entry` as se,
+					`tabItem` as item
+				where se.name = sed.parent
+					and se.posting_date = '{date}'
+					and sed.item_code = item.name
+					and se.scrap_entry = 1
+					and se.pni_reference_type = "Workstation"
+					and sed.item_code = "
+				group by se.pni_reference
+		""".format(date=self.date_for_costing))
+		pass
+	
+	def calculate_bottom_scrap(self, raw):
+		pass
