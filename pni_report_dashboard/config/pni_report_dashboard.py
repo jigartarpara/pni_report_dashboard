0a980883daa89d26f0b1df922eb76faced654881
Jigar Tarpara <jigartarpara68@gmail.com>
2020-12-07 13:13:03 +0530

    Chages

--- pni_report_dashboard/config/pni_report_dashboard.py
+++ pni_report_dashboard/config/pni_report_dashboard.py
@@ -14,5 +14,17 @@ def get_data():
 					"reference_doctype": "Item Price",
 				}
 			]
+		},
+		{
+			"label": _("Sales Person"),
+			"items": [
+				{
+					"type": "report",
+					"name": "Sales Person Sales Analytics",
+					"description": _("Sales Person Sales Analytics"),
+					"is_query_report": True,
+					"reference_doctype": "Sales Order",
+				}
+			]
 		}
 	]
